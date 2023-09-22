e469833b8d471d1d80f629cab41a8898da74e005
shubhammali929 <shubhammali929@gmal.com>
2023-09-22 23:12:33 +0530

    Updated UI

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -6,9 +6,9 @@ export default function Buttons() {
     <div className='ButtonGroup'>
       <Button text="Fix Bug" icon="fa-solid fa-bug-slash"/>
       <Button text="Optimise" icon="fa-solid fa-seedling"/>
-      <Button text="Add Comments" icon="fa-solid fa-code fa-beat-fade"/>
-      <Button text="Change Var names" icon="fa-solid fa-i fa-fade"/>
-      <Button text="Fix indentation" icon="fa-solid fa-toolbox fa-fade"/>
+      <Button text="Add Comments" icon="fa-solid fa-code "/>
+      <Button text="Change Var names" icon="fa-solid fa-i "/>
+      <Button text="Fix indentation" icon="fa-solid fa-toolbox "/>
     </div>
   )
 }
