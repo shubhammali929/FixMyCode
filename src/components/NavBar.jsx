39e4e382efec3808e0cf3584a2839f45f07de225
shubhammali929 <shubhammali929@gmail.com>
2023-10-15 19:55:18 +0530

    deleted unused files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const handleUserIconClick = () => {
 
   return (
     <div className='navbar'>
-      <div className="infoIcon"><i className="fa-solid fa-circle-info"></i></div> 
+      <div className="infoIcon" onClick={() => {alert("Welcome to FixMyCode! Your go-to platform for instant code improvement. Simply enter your code, choose from various optimization options, and let our advanced AI assist you in enhancing your code quality. Experience hassle-free coding with FixMyCode, where innovation meets efficiency")}}><i className="fa-solid fa-circle-info"></i></div> 
       <span className='title'>FixMyCode...</span>
       <div className="userIcon" onClick={handleUserIconClick}><i className="fa-solid fa-user"></i> <i className="fa-solid fa-right-from-bracket" ></i></div> 
     </div>
