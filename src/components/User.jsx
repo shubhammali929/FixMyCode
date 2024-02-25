6476b71b0b0948aa1e114fb37649b36cfeea9939
Shubham Mali <59464210+shubhammali929@users.noreply.github.com>
2024-02-25 12:43:50 +0530

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,9 +4,13 @@ import { useFirebase} from '../context/Firebase';
 export default function User() {
   const firebase = useFirebase();
   const userName = firebase.user?.displayName || firebase.user?.email;
+  const userImage = firebase.user?.photoURL; 
+
   return (
     <div className='user'>
-     <i className="fa-solid fa-user"></i> 
+     {/* <i className="fa-solid fa-user"></i>  */}
+     {userImage && <img src={userImage} alt="User" className="user-image" />}
+      {!userImage && <i className="fa-solid fa-user"></i>}
      <span>{firebase.user?userName:"TempUser"}</span>
     
     </div>
