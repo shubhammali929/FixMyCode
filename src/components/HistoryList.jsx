1be13ee5073724990ebe3da6a181670a2d3f881c
Shubham Mali <59464210+shubhammali929@users.noreply.github.com>
2024-02-25 10:58:22 +0530

    Update HistoryList.jsx

--- src/components/HistoryList.jsx
+++ src/components/HistoryList.jsx
@@ -31,7 +31,7 @@ export default function HistoryList() {
             <History
               key={hist.id}
               id={hist.id}
-              text={hist.data().code.substring(0, 30) + " ... " + hist.id}
+              text={hist.data().code.substring(0, 30) + " ... "}
             />
           ))}
         </div>
